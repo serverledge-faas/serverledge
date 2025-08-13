4171c9566f5feac10da9e9e3602d5df4599f3983
Gabriele Russo Russo <gabri.russo17@gmail.com>
2025-08-13 16:05:58 +0200

    Fixes tests

--- internal/registration/registry.go
+++ internal/registration/registry.go
@@ -68,10 +68,10 @@ func registerToEtcd(asLoadBalancer bool) error {
 		return UnavailableClientErr
 	}
 
-	ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 	resp, err := etcdClient.Grant(ctx, etcdLeaseTTL)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Could not grant lease: %v", err)
 		return err
 	}
 	etcdLease = resp.ID
