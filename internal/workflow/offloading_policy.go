b8fc6d2cd4ce4a5f226f5eec61dd73dd4da97444
Gabriele Russo Russo <gabri.russo17@gmail.com>
2025-09-16 11:33:35 +0200

    Drops unused SimpleOffloadingPolicy

--- internal/workflow/offloading_policy.go
+++ internal/workflow/offloading_policy.go
@@ -20,27 +20,3 @@ func (policy *NoOffloadingPolicy) Evaluate(r *Request, p *Progress) (OffloadingD
 
 	return OffloadingDecision{Offload: false}, nil
 }
-
-type SimpleOffloadingPolicy struct{}
-
-func (policy *SimpleOffloadingPolicy) Evaluate(r *Request, p *Progress) (OffloadingDecision, error) {
-
-	completed := 0
-
-	if p == nil || !r.CanDoOffloading || len(p.ReadyToExecute) == 0 {
-		return OffloadingDecision{Offload: false}, nil
-	}
-
-	for _, s := range p.Status {
-		if s == Executed {
-			completed++
-		}
-	}
-
-	if completed >= 2 && completed < 4 {
-		plan := OffloadingPlan{ToExecute: p.ReadyToExecute} // TODO
-		return OffloadingDecision{true, "127.0.0.1:1323", plan}, nil
-	}
-
-	return OffloadingDecision{Offload: false}, nil
-}
